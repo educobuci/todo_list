c04b4c79a2a591019c9d1f2a452ee4281e9b0de3
Eduardo Cobuci <educobuci@gmail.com>
2011-03-31 01:42:45 -0300

    spec refactoring

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,13 +5,17 @@ describe List do
     @user = Factory.create(:user)
   end
   
-  it "filters the public lists" do
-    List.publics.size.should be==1
-    List.publics.first.public.should be==1
+  describe "publics" do
+    it "returns only the public lists" do
+      List.publics.size.should be==1
+      List.publics.first.public.should be==1
+    end
   end
   
-  it "excludes a given user" do
-    @other = Factory.create(:some_user)
-    List.excluding_user(@other.id).size.should be==2
+  describe "excluding user" do
+    it "excludes the lists from a given user" do
+      @other = Factory.create(:some_user)
+      List.excluding_user(@other.id).size.should be==2
+    end
   end
 end
