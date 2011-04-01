26c46ac458802edbd1e3066c64b48256b3db8d0d
Eduardo Cobuci <educobuci@gmail.com>
2011-04-01 16:07:24 -0300

    fix for the view error

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-ActionView::Base.field_error_proc = proc { |input, instance| input }
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TodoList::Application.initialize!
+
+ActionView::Base.field_error_proc = proc { |input, instance| input }
